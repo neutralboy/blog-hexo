88ac045c2ee1da5001ff6fc6fe4f37c7e65cd674
neutralboy <leopoorna99@gmail.com>
2021-08-21 19:56:57 +0530

    addded say-hello page

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,13 +2,20 @@ import Link from "next/link";
 
 const Header = () => {
     return(
-        <nav className="flex justify-between py-4 px-11 sticky top-0 shadow-xl w-full z-50 text-white font-display text-xl bg-gray-900">
+        <nav className="flex justify-between py-4 px-6 sticky top-0 shadow-xl w-full z-50 text-white font-display text-xl bg-gray-900">
             <div>
                 <Link href="/">
                     <a className="bg-indigo-600 p-2 text-white border-solid border-white border-b-2 inline-block">Poorna's blog</a>
                 </Link>
             </div>
-            <div className="flex justify-around flex-row">
+            <div className="my-auto lg:hidden">
+                <a rel="noopener" href="https://www.linkedin.com/in/poornachandra-v/" className="p-0.5 bg-blue-500 block" >
+                    <svg className="fill-current text-white block" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M13 21H9V9H13V11C13.8526 9.91525 15.1456 9.26857 16.525 9.237C19.0056 9.25077 21.0072 11.2694 21 13.75V21H17V14.25C16.84 13.1326 15.8818 12.3036 14.753 12.306C14.2593 12.3216 13.7932 12.5378 13.4624 12.9046C13.1316 13.2715 12.9646 13.7573 13 14.25V21ZM7 21H3V9H7V21ZM5 7C3.89543 7 3 6.10457 3 5C3 3.89543 3.89543 3 5 3C6.10457 3 7 3.89543 7 5C7 5.53043 6.78929 6.03914 6.41421 6.41421C6.03914 6.78929 5.53043 7 5 7Z"></path>
+                    </svg>
+                </a>
+            </div>
+            <div className="flex justify-around flex-row lg:space-x-3">
                 <div className="p-2">
                     <Link href="/say-hello">
                         <a>
@@ -18,6 +25,11 @@ const Header = () => {
                         </a>
                     </Link>
                 </div>
+                <a rel="noopener" href="https://www.linkedin.com/in/poornachandra-v/" className="p-0.5 bg-blue-500 lg:block hidden my-auto" >
+                    <svg className="fill-current text-white block" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M13 21H9V9H13V11C13.8526 9.91525 15.1456 9.26857 16.525 9.237C19.0056 9.25077 21.0072 11.2694 21 13.75V21H17V14.25C16.84 13.1326 15.8818 12.3036 14.753 12.306C14.2593 12.3216 13.7932 12.5378 13.4624 12.9046C13.1316 13.2715 12.9646 13.7573 13 14.25V21ZM7 21H3V9H7V21ZM5 7C3.89543 7 3 6.10457 3 5C3 3.89543 3.89543 3 5 3C6.10457 3 7 3.89543 7 5C7 5.53043 6.78929 6.03914 6.41421 6.41421C6.03914 6.78929 5.53043 7 5 7Z"></path>
+                    </svg>
+                </a>
             </div>
         </nav>
     )
---
+++ pages/say-hello.tsx
@@ -0,0 +1,41 @@
+import SEOComponent from "../components/SEO";
+
+const SayHello = () => (
+    <div>
+        <SEOComponent title="Say Hello" description="Reach out to me" article={false} />
+        <div style={{backgroundColor:"#111827",backgroundImage:"url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3E%3Cg fill-rule='evenodd'%3E%3Cg fill='%234f46e5' fill-opacity='0.64'%3E%3Cpath opacity='.5' d='M96 95h4v1h-4v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9zm-1 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9z'/%3E%3Cpath d='M6 5V0H5v5H0v1h5v94h1V6h94V5H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E\")"}} className="lg:p-20 p-6">
+            <h1 className="lg:text-7xl text-5xl text-white font-display">Hi, I'm <span className="text-blue-400">Poornachandra</span>.</h1>
+            <h2 className="text-2xl text-gray-200 mt-3"> Medical Student, Data Analyst, Cloud Consultant </h2>
+        </div>
+        <div className="bg-gray-800 lg:p-16 p-10 text-xl w-full">
+            <div>
+                <p className="text-gray-100">
+                    I'm a medical student who enjoys programming and learning the latest tech. I have experience in tech from Wordpress - NGINX to Elasticsearch. Kibana & Logstash. All of my code is on GitHub and hosted on AWS.     
+                </p>
+                <p className="text-gray-100 mt-3">
+                    You can find all('most) of my work on LinkedIn.
+                 </p>
+                 <h3 className="text-2xl mt-4 text-white">If you have anything interesting I can work on you can reach out to me at </h3>
+                 <div className="flex flex-row mt-6 space-x-3"> 
+                    <a rel="noopener" href="https://www.linkedin.com/in/poornachandra-v/" className="flex bg-blue-600 p-3 rounded text-white space-x-3 hover:bg-blue-500">
+                        <svg className="fill-current text-white block" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M13 21H9V9H13V11C13.8526 9.91525 15.1456 9.26857 16.525 9.237C19.0056 9.25077 21.0072 11.2694 21 13.75V21H17V14.25C16.84 13.1326 15.8818 12.3036 14.753 12.306C14.2593 12.3216 13.7932 12.5378 13.4624 12.9046C13.1316 13.2715 12.9646 13.7573 13 14.25V21ZM7 21H3V9H7V21ZM5 7C3.89543 7 3 6.10457 3 5C3 3.89543 3.89543 3 5 3C6.10457 3 7 3.89543 7 5C7 5.53043 6.78929 6.03914 6.41421 6.41421C6.03914 6.78929 5.53043 7 5 7Z"></path>
+                        </svg>
+                        <span className="block font-display text-xl">LinkedIn</span>
+                    </a>
+
+                    <a rel="noopener" href="https://github.com/neutralboy/" className="flex bg-blue-600 p-3 rounded text-white space-x-3 hover:bg-blue-500">
+                        <svg className="fill-current text-white block"width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M12.026 2C7.13295 1.99937 2.96183 5.54799 2.17842 10.3779C1.395 15.2079 4.23061 19.893 8.87302 21.439C9.37302 21.529 9.55202 21.222 9.55202 20.958C9.55202 20.721 9.54402 20.093 9.54102 19.258C6.76602 19.858 6.18002 17.92 6.18002 17.92C5.99733 17.317 5.60459 16.7993 5.07302 16.461C4.17302 15.842 5.14202 15.856 5.14202 15.856C5.78269 15.9438 6.34657 16.3235 6.66902 16.884C6.94195 17.3803 7.40177 17.747 7.94632 17.9026C8.49087 18.0583 9.07503 17.99 9.56902 17.713C9.61544 17.207 9.84055 16.7341 10.204 16.379C7.99002 16.128 5.66202 15.272 5.66202 11.449C5.64973 10.4602 6.01691 9.5043 6.68802 8.778C6.38437 7.91731 6.42013 6.97325 6.78802 6.138C6.78802 6.138 7.62502 5.869 9.53002 7.159C11.1639 6.71101 12.8882 6.71101 14.522 7.159C16.428 5.868 17.264 6.138 17.264 6.138C17.6336 6.97286 17.6694 7.91757 17.364 8.778C18.0376 9.50423 18.4045 10.4626 18.388 11.453C18.388 15.286 16.058 16.128 13.836 16.375C14.3153 16.8651 14.5612 17.5373 14.511 18.221C14.511 19.555 14.499 20.631 14.499 20.958C14.499 21.225 14.677 21.535 15.186 21.437C19.8265 19.8884 22.6591 15.203 21.874 10.3743C21.089 5.54565 16.9181 1.99888 12.026 2Z"></path>
+                        </svg>
+
+                        <span className="block font-display text-xl">GitHub</span>
+                    </a>
+
+                 </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default SayHello;
