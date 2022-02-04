fbce43a3728ba906af313ee888a9fe65a7e6132b
neutralboy <leopoorna99@gmail.com>
2022-02-04 08:59:47 +0530

    written complete shell script

--- scripts/create_blog_post.sh
+++ scripts/create_blog_post.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 slugify () {
-    echo "$1" | iconv -c -t ascii//TRANSLIT | sed -E 's/[~^]+//g' | sed -E 's/[^a-zA-Z0-9]+/-/g' | sed -E 's/^-+|-+$//g' | tr A-Z a-z
+    echo "$@" | iconv -c -t ascii//TRANSLIT | sed -E 's/[~^]+//g' | sed -E 's/[^a-zA-Z0-9]+/-/g' | sed -E 's/^-+|-+$//g' | tr A-Z a-z
 }
 
 relative_location=./../_posts/
-filepath=$(cd "$(dirname "$relative_location")"; pwd)/$(basename "$relative_location")
+filepath=$(cd "$(dirname "$relative_location")"; pwd)/poorna/$(basename "$relative_location")
 NOW=$(date +"%Y-%m-%d %H:%M:%S")
-file_name=$(slugify "$1")
+file_name=$(slugify "$@")
 echo "Time: $NOW"
-echo "Blog Title: $1"   
-echo $filepath/$file_name
+echo "Blog Title: $@"   
+touch "$filepath/$file_name.md"
 
 # Template
-cat "$filepath/$file_name.md" <<EOF 
+cat > "$filepath/$file_name.md" << EOF 
 ---
-title: $1
+title: $@
 date: $NOW
 keywords:
 level: Easy
